07b1f7a303f54764886262206f1803fbd7245dbb
Kev Burns <kevburnsjr@gmail.com>
2019-11-11 20:18:21 -0800

    Simplifying example

--- examples/example.go
+++ examples/example.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 	"os"
 	"strings"
@@ -10,78 +9,41 @@ import (
 	"github.com/kevburnsjr/batchy"
 )
 
-type unbatchedHandler struct{}
-
-func (h unbatchedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := appendIdToFile(r.FormValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-}
-
-type batchedHandler struct {
-	batcher batchy.Batcher
-}
-
-func (h batchedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := h.batcher.Add(r.FormValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-}
 
 func main() {
-	http.Handle("/unbatched", unbatchedHandler{})
-	http.Handle("/batched", batchedHandler{
-		// Max batch size 100
-		// Max wait time 100 milliseconds
-		batchy.New(100, 100*time.Millisecond, func(items []interface{}) (errs []error) {
-			var ok bool
-			errs = make([]error, len(items))
-			var ids = make([]string, len(items))
-			for i, v := range items {
-				ids[i], ok = v.(string)
-				if !ok {
-					errs[i] = errors.New("Wrong data type")
-				}
+	// Max batch size 100
+	// Max wait time 100 milliseconds
+	batcher := batchy.New(100, 100*time.Millisecond, func(items []interface{}) (errs []error) {
+		errs = make([]error, len(items))
+		var ids = make([]string, len(items))
+		for i, v := range items {
+			ids[i] = v.(string)
+		}
+		err := appendToFile(strings.Join(ids, "\n"))
+		if err != nil {
+			for i := range items {
+				errs[i] = err
 			}
-			err := appendIdsToFile(ids)
-			if err != nil {
-				for i := range items {
-					errs[i] = err
-				}
-			}
-			return
-		}),
+		}
+		return
+	})
+	http.HandleFunc("/batched", func(w http.ResponseWriter, r *http.Request) {
+		batcher.Add(r.FormValue("id"))
+	})
+	http.HandleFunc("/unbatched", func(w http.ResponseWriter, r *http.Request) {
+		appendToFile(r.FormValue("id"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
-func appendIdsToFile(ids []string) (err error) {
-	if len(ids) == 0 {
-		return
-	}
+func appendToFile(str string) (err error) {
 	f, err := os.OpenFile("items", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0600)
 	if err != nil {
 		return
 	}
 	defer f.Close()
 
-	_, err = f.Write([]byte(strings.Join(ids, "\n") + "\n"))
-	if err != nil {
-		return
-	}
-	return
-}
-
-func appendIdToFile(id string) (err error) {
-	f, err := os.OpenFile("items", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0600)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-
-	_, err = f.Write([]byte(id + "\n"))
+	_, err = f.Write([]byte(str + "\n"))
 	if err != nil {
 		return
 	}
